fa0bd97ebf02ef4b2b8636288ef7a2fec989b3c4
zubair <zubairhasrat48@gmail.com>
2024-09-18 19:24:07 +0500

    Initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,29 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import db from "@/db";
+import { env } from "@/env/server";
+
+const options: NextAuthOptions = {
+  pages: {
+    signIn: "/",
+  },
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  adapter: DrizzleAdapter(db),
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default options;
---
+++ src/env/server.ts
@@ -0,0 +1,37 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { ZodError, z } from "zod";
+
+expand(config());
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]),
+    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_SECRET: z.string(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    DB_HOST: z.string(),
+    DB_USER: z.string(),
+    DB_PASSWORD: z.string(),
+    DB_NAME: z.string(),
+    DB_PORT: z.coerce.number(),
+    DATABASE_URL: z.string().url(),
+    DB_MIGRATING: z
+      .string()
+      .refine((s) => s === "true" || s === "false")
+      .transform((s) => s === "true")
+      .optional(),
+  },
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors
+    );
+    process.exit(1);
+  },
+  emptyStringAsUndefined: true,
+  // eslint-disable-next-line n/no-process-env
+  experimental__runtimeEnv: process.env,
+});
